92e397b8f3f4247f41a0be31738b7b0568323869
Eric Lemoine <ratibus@gmail.com>
2021-12-13 22:21:08 +0100

    2021 Day 13 code cleaning

--- 2021/day_13/solve_02.php
+++ 2021/day_13/solve_02.php
@@ -16,38 +16,21 @@ foreach (file($inputFile, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES) as $y =
         $folds[] = ['axis' => $foldAxis, 'coord' => $foldCoord];
     } else {
         list($x, $y) = explode(',', $line);
-        $grid[$y][$x] = true;
+        $grid[$y][$x] = $x;
     }
 }
 
 foreach ($folds as $fold) {
-
     $newGrid = [];
 
-    if ($fold['axis'] === 'y') {
-
-        foreach ($grid as $y => $row) {
-
-            if ($y == $fold['coord']) {
-                continue; // skip line on fold axis
-            }
-
-            foreach ($row as $x => $true) {
+    foreach ($grid as $y => $row) {
+        foreach ($row as $x) {
+            if ($fold['axis'] === 'y') {
                 $newY = $y < $fold['coord'] ? $y : 2 * $fold['coord'] - $y;
-                $newGrid[$newY][$x] = true;
-            }
-        }
-
-    } else {
-
-        foreach ($grid as $y => $row) {
-            foreach ($row as $x => $true) {
-
-                if ($x == $fold['coord']) {
-                    continue; // skip column on fold axis
-                }
+                $newGrid[$newY][$x] = $x;
+            } else {
                 $newX = $x < $fold['coord'] ? $x : 2 * $fold['coord'] - $x;
-                $newGrid[$y][$newX] = true;
+                $newGrid[$y][$newX] = $newX;
             }
         }
     }
